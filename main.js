8475d0c6fc0880c355dc7eb1988eeeb86b876375
Samson <samsondimitrijevic@gmail.com>
2024-04-04 11:26:18 +0200

    second commit

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <div>
    <h1>Hello Netlify</h1>
+   <h3>A RockTheCode lesson</h3>
    <p>An example project for deploying on netlify</p>
   </div>
 `
